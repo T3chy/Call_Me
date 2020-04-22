56b8b02ff7625272407e54dde334575837134bca
t3chy <elam.day.friedland@gmail.com>
2020-04-22 16:02:01 -0700

    got url getter working but now need to fetch all of the actual transcript

--- get_transripts.py
+++ get_transripts.py
@@ -1,6 +1,8 @@
 from bs4 import BeautifulSoup
 import requests
 import re
+import pandas
+import os
 def gettranscriptlinks(ticker):
     links = []
     request = requests.get('http://www.conferencecalltranscripts.org/?co=' + ticker)
@@ -9,5 +11,14 @@ def gettranscriptlinks(ticker):
     transcripts = transcripts[0].findAll('a')
     for i in transcripts:
         links.append([i.get_text(),(re.search('location=(.*)',i['href']).group(0).strip('location='))])
-    return(links)
-print(gettranscriptlinks('TSLA'))
+    return([ticker,links])
+def downloadtranscripts(links):
+    try:
+        os.mkdir(links[0])
+        os.chdir(links[0])
+    except OSError:
+        os.chdir(links[0])
+    for i in range(10):
+        with open(links[1][i+1][0] + '.txt','w') as f:
+            f.write(requests.get(links[1][i+1][1]).text)
+downloadtranscripts(gettranscriptlinks('TSLA'))
